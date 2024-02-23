f898301f220c8600c49bbdf833aefd65d8d301ae
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-02-23 01:09:12 -0300

    enable auto_install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "markdown", "markdown_inline" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
