c38a2bb096d0faf55b051f139fa1877d8b45e3fa
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-07-12 00:53:09 -0300

    use dark GTK theme for Emacs decorations

--- install.sh
+++ install.sh
@@ -225,6 +225,14 @@ configure_gnome() {
     else
         gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
     fi
+
+    if ! gsettings list-schemas | grep -Fxq org.gnome.desktop.interface; then
+        warn "Schema de interface do GNOME não encontrado; tema GTK não foi configurado."
+    elif [[ "$(gsettings get org.gnome.desktop.interface gtk-theme)" == "'Adwaita-dark'" ]]; then
+        printf 'Ignorando tema GTK: Adwaita-dark já está configurado.\n'
+    else
+        gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+    fi
 }
 
 finish() {
